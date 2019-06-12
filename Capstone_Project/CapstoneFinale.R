d0a4e10e9f8262d38c7729a5b4fe5eaf5240e2bf
jslaga <36487954+jslaga@users.noreply.github.com>
2019-06-12 00:32:59 -0700

    Update CapstoneFinale.R

--- Capstone_Project/CapstoneFinale.R
+++ Capstone_Project/CapstoneFinale.R
@@ -1,12 +1,10 @@
 #install and load packages
 install.packages( "tidyr")
-install.packages("randomForest")
 install.packages("maps")
 
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(randomForest)
 library(scales)
 library(maps)
 
@@ -100,7 +98,7 @@ str(late_flights)
 total_by_day <- flights_df %>% group_by(DAY_OF_WEEK) %>% tally
 total_by_month <- flights_df %>% group_by(MonthName) %>% tally
 total_by_airline <- flights_df %>% group_by(AIRLINE) %>% tally
-##  ??NAME COLUMSN???*************
+
 late_by_day <- late_flights %>% group_by(DAY_OF_WEEK) %>% tally 
 late_by_month <- late_flights %>% group_by(MonthName) %>% tally
 late_by_airline <- late_flights %>% group_by(AIRLINE) %>% tally
@@ -117,11 +115,15 @@ total_by_daypart <- late_flights %>% group_by(DAYPART) %>% tally
 
 ## flights by day of week
 ggplot(late_flights, aes(x = DAY_OF_WEEK)) + 
-  geom_bar()
+  geom_bar(fill = "#37004D") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title="Delayed Flights By Day", x ="Day Of Week", y = "Delayed Flights")
 
 # Days of month
 ggplot(late_flights, aes(x = MonthName)) + 
-  geom_bar()
+  geom_bar(fill = "#AA4371") +
+  theme(plot.title = element_text(hjust = 0.5), axis.text.x = element_text(angle = 60, hjust = 1)) +
+  labs(title="Delayed Flights By Month", x ="Month", y = "Delayed Flights")
   
   
 ##examine only top 10 airlines; US has incomplete data; used AS instead
@@ -131,32 +133,44 @@ late_flights <- late_flights %>% filter(AIRLINE %in% included)
 
 ## distribution of departure delays
 ggplot(late_flights, aes(x = DEPARTURE_DELAY)) +
-  geom_histogram(binwidth = 15) +
-  scale_x_continuous(limits = c(0, 500))
+  geom_histogram(binwidth = 15, fill = "#0000FF") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_x_continuous(limits = c(0, 500)) +
+  labs(title="Distribution Of Departure Delays", x ="Delays (Minutes)")
 
-ggplot(late_flights, aes(x = DEPARTURE_DELAY)) +
+ggplot(late_flights, aes(x = DEPARTURE_DELAY, fill = AIRLINE)) +
   geom_histogram(binwidth = 15) +
   scale_x_continuous(limits = c(0, 500)) +
-  facet_grid(cols = vars(AIRLINE))
+  scale_y_continuous(limits = c(0, 150000)) + 
+  theme(plot.title = element_text(hjust = 0.5), axis.text.x = element_blank(), axis.ticks = element_blank()) +
+  facet_grid(cols = vars(AIRLINE)) +
+  labs(title="Distribution Of Departure Delays By Airline", x ="Delays (Minutes)")
 
 delay_totals <- late_flights %>% group_by(DEPARTURE_DELAY) %>% 
   tally %>% arrange(desc(n))
 # direct correlation between length of departure delay and frequency
 
-ggplot(late_flights, aes(AIRLINE, DEPARTURE_DELAY)) +
-  geom_boxplot()
+ggplot(late_flights, aes(AIRLINE, DEPARTURE_DELAY, fill = AIRLINE)) +
+  geom_boxplot(outlier.shape = 16,outlier.size = 0.5) + 
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none") +
+  labs(title="Delays By Airline", y ="Delays (Minutes)")
+
 # a majority of the delays are brief; American Airlines has the longest delays
 
 ##examine short and long delays
 short <- filter(late_flights, DEPARTURE_DELAY < 60)
 extreme <- filter(late_flights, DEPARTURE_DELAY >600) #10+ hours
 
+ggplot(short, aes(AIRLINE, DEPARTURE_DELAY, fill = AIRLINE)) +
+  geom_boxplot(outlier.shape = 16,outlier.size = 1) + 
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none") +
+  labs(title="Short Delays (< 60 minutes)", y ="Delays (Minutes)")
 
-ggplot(short, aes(AIRLINE, DEPARTURE_DELAY)) +
-  geom_boxplot()
+ggplot(extreme, aes(AIRLINE, DEPARTURE_DELAY, fill = AIRLINE)) +
+  geom_boxplot(outlier.shape = 16,outlier.size = 1) + 
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none") +
+  labs(title="Extreme Delays (10+ hours)", y ="Delays (Minutes)")
 
-ggplot(extreme, aes(AIRLINE, DEPARTURE_DELAY)) +
-  geom_boxplot()
 
 ##look at weather delays
 ##scaled to normalize across the airlines regardless of number of flights
@@ -167,38 +181,35 @@ weather_delays$Proportion <- weather_delays$Total_Time/sum(weather_delays$Total_
 
 ggplot(weather_delays) +
   geom_line(aes(x=weather_delays$`(MONTH)`, y=weather_delays$Proportion, color = AIRLINE)) +
-  labs(title = "Weather Delays By Month", x = "Month", y = "Minutes Delayed") +
+  labs(title = "Monthly Weather Delays Per Airline", x = "Month", y = "Proportion Of Minutes Delayed") +
   theme_bw() +
   theme(axis.text.x = element_text(angle=45,vjust=0.5), 
-        plot.title = element_text(hjust=0.5)) +
+        plot.title = element_text(hjust=0.5), legend.position = "none") +
   scale_x_continuous(breaks = pretty_breaks(12)) +
   facet_wrap(~AIRLINE)
 
-#######
+##weather delays by season
 summer_delays <- late_flights %>% subset(WEATHER_DELAY > 0) %>% subset(MONTH %in% c(5, 6, 7, 8))
 
 nonsummer_delays <- late_flights %>% subset(WEATHER_DELAY > 0) %>% subset(!MONTH %in% c(5, 6, 7, 8))
 
 delay_df <- late_flights %>% subset(WEATHER_DELAY > 0) %>% mutate(Season = ifelse(MONTH %in% c(5, 6, 7, 8), "summer", "winter"))
 
-ggplot(delay_df, aes(delay_df$Season, delay_df$WEATHER_DELAY)) +
+ggplot(delay_df, aes(delay_df$Season, delay_df$WEATHER_DELAY, fill = delay_df$Season)) +
   geom_boxplot(outlier.shape = NA) +
-  scale_y_continuous(limits = c(0,150))
-
+  scale_y_continuous(limits = c(0,150)) +
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none") +
+  labs(title = "Weather Delays By Season", x = "Season", y = "Weather Delays") 
+  
 ## t test averages between summer and non summer delays
 t.test(summer_delays$WEATHER_DELAY, nonsummer_delays$WEATHER_DELAY)
 ## while there are more delays in summer, there's no sig diff in average delay througout the year; 
 ## p-value = 0.09289 ; ~10% chance that pops are same 
 
 
-
-#############################################################
 ##where are the airports with the delays?
-##looking at SW summer months  May. june July
-WN_summer <- late_flights %>% subset(WEATHER_DELAY > 0) %>% subset(AIRLINE == "WN") %>% 
-  subset(MONTH %in% c(5, 6, 7, 8)) %>% group_by(ORIGIN_AIRPORT) %>%
-  summarise(Total_Time = sum(WEATHER_DELAY)) 
 
+##get latitude and longitude info for airlports
 target_airlines <- c("WN", "DL", "UA", "AA", "OO", "EV", "B6", "AS", "MQ", "NK")
 
 target_df <- flights_df %>% filter(AIRLINE %in% target_airlines)
@@ -228,33 +239,50 @@ us <- map_data("state")
 
 ggplot() +  
   geom_map(aes(x = long, y = lat, map_id = region), data = us,
-           map = us, fill = "88", size = 0.15, color = "black") +
+           map = us, fill = "#FF9933", size = 0.15, color = "black") +
   geom_point(data = map_df, 
-             aes(x = LONG, y = LAT,  colour = map_df$Total_Time/sum(map_df$Total_Time)),
-             shape = 16) +
-  scale_color_gradient(low="beige", high="blue")
+             aes(x = LONG, y = LAT,  colour = map_df$Total_Time/sum(map_df$Total_Time)), shape = 16) +
+  scale_color_gradient(low="beige", high="blue") +
+  scale_x_continuous(limits = c(-130,-60))  +
+  theme(legend.title = element_blank()) +
+  labs(title = "Proportion Of Flight Time Delays By Airport", x = "Longitude", y = "Latitude")
+
+
+# Create Training and Test data -
+set.seed(666)  
+split <- sample(1:nrow(late_flights), 0.8*nrow(late_flights))  # row indices for training data
+trainingData <- late_flights[split, ]  # model training data
+testData  <- late_flights[-split, ]   # test data
 
 
 ## Linear regression models
-model1 <- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY, data=late_flights)
-summary(model1)
 
+## Model 1
+model1 <- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY + DAYPART, data=late_flights)
+summary(model1)
+ 
+## Model 2 **
 model2 <- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY + DEPARTURE_DELAY:DAYPART, data=late_flights)
 summary(model2)
 
-model3 <- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY + DAYPART + DAY_OF_WEEK, data=flights_df)
+## Model 3 **
+model3 <- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY + DAY_OF_WEEK + DEPARTURE_DELAY:DAY_OF_WEEK, data=late_flights)
 summary(model3)
 
+## Model 4
+model4 <- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY + DEPARTURE_DELAY:DAYPART + DAY_OF_WEEK + MONTH , data=late_flights)
+summary(model4)
 
-#Split the data into training dataset and testing dataset with a 75/25 ratio.
-set.seed(666)
-split <- sample(nrow(late_flights), floor(0.75*nrow(late_flights)))
-Final_train <- late_flights[split,]
-Final_test <- late_flights[-split,]
+## Model 5 
+model5 <- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY + DEPARTURE_DELAY:DAYPART + DAY_OF_WEEK + AIRLINE, data=late_flights)
+summary(model5)
 
-TrainModel<- lm(ARRIVAL_DELAY ~ DEPARTURE_DELAY + DAYPART, data=Final_train)
+## Build the model on training data (Chose Model 2)
+TrainModel<- lm(model2)
+prediction <- predict(TrainModel, testData)
+plot(testData$ARRIVAL_DELAY, prediction)
 
-prediction <- predict(TrainModel, Final_test)
-plot(prediction, Final_test$ARRIVAL_DELAY)
-
-
+## Build the model on training data (Chose Model 3)
+TrainModel<- lm(model3)
+prediction <- predict(TrainModel, testData)
+plot(testData$ARRIVAL_DELAY, prediction)
